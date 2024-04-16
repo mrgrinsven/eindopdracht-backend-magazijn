bf171fcd7be2de203027b8cb599231390c638be0
mrgrinsven <mrgrinsven@gmail.com>
2024-04-16 16:12:18 +0200

    first commit

---
+++ src/main/java/nl/novi/eindopdracht/eindopdrachtbackendmagazijn/EindopdrachtBackendMagazijnApplication.java
@@ -0,0 +1,13 @@
+package nl.novi.eindopdracht.eindopdrachtbackendmagazijn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EindopdrachtBackendMagazijnApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EindopdrachtBackendMagazijnApplication.class, args);
+    }
+
+}
---
+++ src/test/java/nl/novi/eindopdracht/eindopdrachtbackendmagazijn/EindopdrachtBackendMagazijnApplicationTests.java
@@ -0,0 +1,13 @@
+package nl.novi.eindopdracht.eindopdrachtbackendmagazijn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EindopdrachtBackendMagazijnApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
